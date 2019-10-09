9cc6bd0a01636b08c2e3fb4320afad9145dba9be
Javier <blitzexgm@gmail.com>
2019-10-09 11:31:08 +0200

    Descripciones personajes

--- 2DPlatformerTut_Part24/Assets/Scripts/EnemySight.cs
+++ 2DPlatformerTut_Part24/Assets/Scripts/EnemySight.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class EnemySight : MonoBehaviour {
 
+    // Declara al enemigo.
     [SerializeField]
     private Enemy enemy;
 
+    // Si detecta al player, cambiará su target a Player.
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
@@ -14,6 +16,8 @@ public class EnemySight : MonoBehaviour {
         }
     }
 
+
+    // Si deja de detecta al player, cambiará su target a nulo.
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player")
