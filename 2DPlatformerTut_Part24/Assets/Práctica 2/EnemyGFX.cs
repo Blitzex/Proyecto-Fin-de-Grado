e304d660d9906d46abb26d44017c108c953e8720
Javier <blitzexgm@gmail.com>
2019-10-24 20:51:30 +0200

    Codigo mejorado

---
+++ 2DPlatformerTut_Part24/Assets/Práctica 2/EnemyGFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding; //Siempre que queramos programar algo con A* debemos incluirlo
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aiPath; //Nombre del script que controla al enemigo
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(aiPath.desiredVelocity.x >= 0.01f) //Velocidad a la que el enemigo querrá desplazarse según la ruta que tenga que tomar para alcanzar al jugador
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f); //Si el valor es mayor que 0.01 mirará a la derecha
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f); //Si el valor es menor que 0.01 mirará a la izquierda
+        }
+    }
+}
