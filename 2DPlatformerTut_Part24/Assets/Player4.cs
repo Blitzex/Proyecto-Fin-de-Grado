fb2e54358eb5caf657ef9b259587047dfc06f22a
SrEscopeta <miguelcastillaasir@gmail.com>
2019-10-25 16:33:13 +0200

    Script movimiento

---
+++ 2DPlatformerTut_Part24/Assets/Player4.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player4 : MonoBehaviour
+{
+    public float speed = 4f;
+
+    private void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+        Vector3 mov = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            0
+            );
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            transform.position + mov,
+            speed * Time.deltaTime
+            );
+    }
+}
